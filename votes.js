9d83d3a06597ce39094afde8bbed772bbfb6ddb7
Ilyas Haddad <46970899+summoNN@users.noreply.github.com>
2024-05-04 20:45:49 +0200

    v2

--- votes.js
+++ votes.js
@@ -1,9 +1,20 @@
 import puppeteer from "puppeteer";
+require("dotenv").config();
 
 export const getVotes = async (id) => {
   const browser = await puppeteer.launch({
+    args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
     headless: true,
     defaultViewport: null,
+    executablePath:
+    process.env.NODE_ENV === "production"
+      ? process.env.PUPPETEER_EXECUTABLE_PATH
+      : puppeteer.executablePath(),
   });
   const page = await browser.newPage();
 
