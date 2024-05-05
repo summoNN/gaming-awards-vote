83fee03115fa6a2ae55052989f26b55a889ff27c
Ilyas Haddad <46970899+summoNN@users.noreply.github.com>
2024-05-05 17:36:36 +0200

    enhanced getVotes function perfomances

--- votes.js
+++ votes.js
@@ -2,32 +2,41 @@ import puppeteer from "puppeteer";
 import dotenv from "dotenv";
 dotenv.config();
 
+let browserInstance;
+
+const launchBrowser = async () => {
+  if (!browserInstance) {
+    browserInstance = await puppeteer.launch({
+      args: [
+          "--disable-setuid-sandbox",
+          "--no-sandbox",
+          "--single-process",
+          "--no-zygote",
+        ],
+      headless: true,
+      defaultViewport: null,
+      executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+    });
+  }
+  return browserInstance;
+};
+
 export const getVotes = async (id) => {
-  const browser = await puppeteer.launch({
-    args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-    headless: true,
-    defaultViewport: null,
-    executablePath:
-    process.env.NODE_ENV === "production"
-      ? process.env.PUPPETEER_EXECUTABLE_PATH
-      : puppeteer.executablePath(),
-  });
+  const browser = await launchBrowser();
   const page = await browser.newPage();
 
-  await page.goto(`https://voting.lgamingawards.com/participant?p=${id}`, {
-    waitUntil: "networkidle2",
-  });
+  try {
+    await page.goto(`https://voting.lgamingawards.com/participant?p=${id}`, {
+      waitUntil: "networkidle2",
+    });
 
-  const searchResultSelector = '.baTaHaFv > div';
-  await page.waitForSelector(searchResultSelector);  
-  const votes = await page.$eval(searchResultSelector, el => el.textContent)
+    const votes = await page.$eval('.baTaHaFv > div', el => el.textContent);
 
-  await browser.close();
-
-  return votes
+    return votes;
+  } finally {
+    await page.close();
+  }
 };
